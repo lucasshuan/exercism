9c42b76a1df7c3898d6d971ba370883ad4be2d92
Lucas Rocha <78228526+lucroch@users.noreply.github.com>
2023-02-18 19:21:56 -0300

    go: Bird Watcher

--- go/bird-watcher/bird_watcher.go
+++ go/bird-watcher/bird_watcher.go
@@ -3,17 +3,26 @@ package birdwatcher
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-	panic("Please implement the TotalBirdCount() function")
+	var birdsTotal int
+	for i := 0; i < len(birdsPerDay); i++ {
+		birdsTotal += birdsPerDay[i]
+	}
+	return birdsTotal
 }
 
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	w := (week-1) * 7
+	birdsThatWeek := birdsPerDay[w:w+7]
+	return TotalBirdCount(birdsThatWeek)
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i]++
+	}
+	return birdsPerDay
 }
