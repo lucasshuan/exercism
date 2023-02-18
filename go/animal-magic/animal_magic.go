c12b6ad346a17c66255a02a02c2ebcdd21c706f4
Lucas Rocha <78228526+lucroch@users.noreply.github.com>
2023-02-18 18:43:11 -0300

    go: Bird Watcher

---
+++ go/animal-magic/animal_magic.go
@@ -0,0 +1,30 @@
+package chance
+
+import (
+	"math/rand"
+	"time"
+)
+
+// SeedWithTime seeds math/rand with the current computer time.
+func SeedWithTime() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RollADie returns a random int d with 1 <= d <= 20.
+func RollADie() int {
+	return rand.Intn(19) + 1
+}
+
+// GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
+func GenerateWandEnergy() float64 {
+	return float64(rand.Intn(12)) + rand.Float64()
+}
+
+// ShuffleAnimals returns a slice with all eight animal strings in random order.
+func ShuffleAnimals() []string {
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Shuffle(len(animals), func(i, j int) {
+		animals[i], animals[j] = animals[j], animals[i]
+	})
+	return animals
+}
