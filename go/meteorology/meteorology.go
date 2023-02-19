bb53a6c04b724be0d7dec3add04bbb3b94f354e0
Lucas Rocha <78228526+lucroch@users.noreply.github.com>
2023-02-19 14:10:04 -0300

    go: Meteorology

---
+++ go/meteorology/meteorology.go
@@ -0,0 +1,69 @@
+package meteorology
+
+import "fmt"
+
+type TemperatureUnit int
+
+const (
+	Celsius    TemperatureUnit = 0
+	Fahrenheit TemperatureUnit = 1
+)
+
+// Add a String method to the TemperatureUnit type
+func (sc TemperatureUnit) String() string {
+	units := []string{"°C", "°F"}
+	return units[sc]
+}
+
+type Temperature struct {
+	degree int
+	unit   TemperatureUnit
+}
+
+// Add a String method to the Temperature type
+func (t Temperature) String() string {
+	return fmt.Sprintf("%d %s", t.degree, t.unit.String())
+}
+
+type SpeedUnit int
+
+const (
+	KmPerHour    SpeedUnit = 0
+	MilesPerHour SpeedUnit = 1
+)
+
+// Add a String method to SpeedUnit
+func (sc SpeedUnit) String() string {
+	units := []string{"km/h", "mph"}
+	return units[sc]
+}
+
+type Speed struct {
+	magnitude int
+	unit      SpeedUnit
+}
+
+// Add a String method to Speed
+func (s Speed) String() string {
+	return fmt.Sprintf("%d %s", s.magnitude, s.unit.String())
+}
+
+type MeteorologyData struct {
+	location      string
+	temperature   Temperature
+	windDirection string
+	windSpeed     Speed
+	humidity      int
+}
+
+// Add a String method to MeteorologyData
+func (md MeteorologyData) String() string {
+	return fmt.Sprintf(
+		"%s: %s, Wind %s at %s, %d%% Humidity", 
+		md.location, 
+		md.temperature.String(),
+		md.windDirection,
+		md.windSpeed.String(),
+		md.humidity,
+	)
+}
