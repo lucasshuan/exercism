2f37fe6bc8773e606be8a74302a82c9953c4bdeb
Lucas Rocha <78228526+lucroch@users.noreply.github.com>
2023-02-21 17:20:57 -0300

    go: Two Fer

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,17 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package twofer should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package twofer
+
+import "fmt"
+
+// ShareWith should have a comment documenting it.
+func ShareWith(name string) string {
+	s := name
+	if name == "" {
+		s = "you"
+	}
+	return fmt.Sprintf("One for %s, one for me.", s)
+}
