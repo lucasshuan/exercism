8c9d9a4bff0c2c0ec59e59e7c15e1bd712436339
Lucas Rocha <78228526+lucroch@users.noreply.github.com>
2023-02-21 17:47:06 -0300

    go: Hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,25 @@
 package hamming
 
+import (
+	"errors"
+)
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	var n int
+	if len(a) != len(b) {
+		return 0, errors.New("n")
+	}
+	if a == b || a == "" {
+		return 0, nil
+	}
+	brunes := []rune(b)
+	for i, r := range a {
+		if r != 'C' && r != 'A' && r != 'T' && r != 'G' {
+			continue
+		}
+		if r != brunes[i] {
+			n++
+		}
+	}
+	return n, nil
 }
