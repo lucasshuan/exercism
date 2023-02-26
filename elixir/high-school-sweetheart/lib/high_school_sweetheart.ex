d2376bcb0b70fda3a3e754dc30decfc5bc519748
Lucas Rocha <78228526+lucroch@users.noreply.github.com>
2023-02-26 02:14:46 -0300

    elixir: High School Sweetheart

---
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,38 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    name
+    |> String.trim()
+    |> String.first()
+  end
+
+  def initial(name) do
+    name
+    |> first_letter()
+    |> String.upcase()
+    |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    [name, surname | _] = String.split(full_name)
+    "#{initial(name)} #{initial(surname)}"
+  end
+
+  def pair(full_name1, full_name2) do
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
