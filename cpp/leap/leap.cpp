86ff79c461c534842d1aa7c0b696e6d0051e11e6
Lucas Rocha <78228526+lucroch@users.noreply.github.com>
2023-03-03 11:30:37 -0300

    cpp: Leap

--- cpp/leap/leap.cpp
+++ cpp/leap/leap.cpp
@@ -3,10 +3,6 @@
 namespace leap {
   bool is_leap_year(int year)
   {
-    if (year % 4 == 0)
-    {
-      return true;
-    }
-    return false;
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
   }
 }  // namespace leap
