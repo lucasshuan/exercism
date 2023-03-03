487dfe9d5c486814fc7fe58b6587a99de7d7248d
Lucas Rocha <78228526+lucroch@users.noreply.github.com>
2023-03-03 11:06:45 -0300

    go: Resistor Color

---
+++ go/resistor-color/resistor_color.go
@@ -0,0 +1,27 @@
+package resistorcolor
+
+// Colors should return the list of all colors.
+func Colors() []string {
+	return []string{
+		"black",
+		"brown",
+		"red",
+		"orange",
+		"yellow",
+		"green",
+		"blue",
+		"violet",
+		"grey",
+		"white",
+	}
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	for i, c := range Colors() {
+		if c == color {
+			return i
+		}
+	}
+	return -1
+}
