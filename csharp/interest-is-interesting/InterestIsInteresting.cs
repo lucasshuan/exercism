3065c97fe4937eac74981ede44f5ab60a900eefb
Lucas Rocha <78228526+lucroch@users.noreply.github.com>
2023-06-01 01:32:57 -0300

    csharp: Interest is Interesting update

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -2,27 +2,19 @@ using System;
 
 static class SavingsAccount
 {
-    public static float InterestRate(decimal balance)
-    {
-        if (balance < 0)
-            return 3.213f;
-        else if (balance < 1000)
-            return 0.5f;
-        else if (balance < 5000)
-            return 1.621f;
-        else
-            return 2.475f; 
-    }
+    public static float InterestRate(decimal balance) => 
+        balance switch {
+            < 0 => 3.213f,
+            < 1000 => 0.5f,
+            < 5000 => 1.621f,
+            _ => 2.475f
+        };
 
-    public static decimal Interest(decimal balance)
-    {
-        return balance * ((decimal)InterestRate(balance) / 100);
-    }
+    public static decimal Interest(decimal balance) =>
+        balance * ((decimal)InterestRate(balance) / 100);
 
-    public static decimal AnnualBalanceUpdate(decimal balance)
-    {
-        return balance + Interest(balance);
-    }
+    public static decimal AnnualBalanceUpdate(decimal balance) =>
+        balance + Interest(balance);
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
